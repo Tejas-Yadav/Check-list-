dcade3fbf0c51bd7b2144a9a059c6487334335d5
Tejas Yadav <tejasy919@gmail.com>
2024-08-16 00:22:18 +0530

    Commit 2 (functional)

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,14 +3,14 @@ import  check from '../assets/check.png'
 import  images from '../assets/images.png'
 import bin from '../assets/bin.png'
 
-const Tasks = ({text}) => {
+const Tasks = ({text, id, isComplete, deleteItem, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1 items-center cursor-pointer'>
-            <img className='w-6' src={check} alt="" />
+        <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
+            <img className='w-6' src={isComplete ? check : images } alt="" />
             <p className='ml-4 text-[15px]'>{text}</p>
         </div>
-     <img className='w-7 cursor-pointer' src={bin} alt="" />
+     <img onClick={()=>{deleteItem(id)}}  src={bin} alt="" className='w-7 cursor-pointer'/>
     </div>
   )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,25 +1,71 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import checklist from '../assets/checklist.png'
 import Tasks from './Tasks'
 
 const Todo = () => {
+
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+  const add = () => {
+      const inputText = inputRef.current.value.trim();
+
+      if (inputText === "") {
+        return null;
+      }
+
+      const newTodo = {
+        id: Date.now(),
+        text: inputText,
+        isComplete: false,
+      }
+      setTodoList((prev)=> [...prev,newTodo]);
+      inputRef.current.value = "";
+    }
+
+    const deleteItem = (id) => {
+      setTodoList((prevTodo) =>{
+       return prevTodo.filter((todo)=> todo.id !== id)
+      })
+    }
+
+    const toggle = (id) => {
+      setTodoList((prevTodo)=>{
+        return prevTodo.map((todo=>{
+          if(todo.id === id){
+            return {...todo, isComplete: !todo.isComplete}
+          }
+          return todo;
+        }))
+      });
+    
+    };
+
+useEffect(()=>{
+  console.log(todoList)
+},[todoList])
+
+
   return (
     <div className='bg-white place-self-center w-5/6 max-w-md flex flex-col p-7 min-h-[500px] rounded-3xl shadow-2xl'>
+
      <div className='flex items-center mt-6 gap-2'>
         <img className='w-6'  src={checklist} alt=""/>
         <h1 className='text-2xl font-semibold'>Checklist</h1>
      </div>
+
      <div>
      <div className='flex items-center my-7 bg-green-200 rounded-full shadow-sm'>
-        <input className='flex bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 shadow-lg placeholder:text-slate-600' type='text' placeholder='Add tasks'/>
-        <button className='border-none bg-green-500 h-10 w-1/5 rounded-full font-medium font-sans text-lg cursor-pointer'> ADD +</button>
+        <input ref={inputRef} className='flex bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 shadow-lg placeholder:text-slate-600' type='text' placeholder='Add tasks'/>
+        <button onClick={add} className='border-none bg-green-500 h-10 w-1/5 rounded-full font-medium font-sans text-lg cursor-pointer'> ADD +</button>
     </div>
+
     <div>
-        <Tasks text={'add'}/>
-        <Tasks text={'add more'}/>
-    
+     {todoList.map((item,index)=>{
+        return <Tasks key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteItem={deleteItem} toggle={toggle}/>
+     })}
     </div>
-     </div>
+   </div>
+
     </div>
   )
 }
